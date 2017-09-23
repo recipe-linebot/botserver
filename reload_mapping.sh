4768cd4dd70bab20131cb04c67e698bfacb68a1c
minagoro <minagoro0522@gmail.com>
2017-09-23 15:25:23 +0900

    :fire: Remove all codes excepts botserver

--- reload_mapping.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-RECIPEDB_HOST=127.0.0.1
-RECIPEDB_PORT=9200
-MAPPING_PATH=/opt/recipe-linebot/share/mapping-template.json
-
-curl -XPUT "$RECIPEDB_HOST:$RECIPEDB_PORT/_template/recipe-linebot?pretty" -d @$MAPPING_PATH
