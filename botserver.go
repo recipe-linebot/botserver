d9f7651ab52e9865e2e4357a39750d4c51001ce6
minagoro <minagoro0522@gmail.com>
2017-10-05 20:35:01 +0900

    Split codes that are related to recipe DB into recipedb.go

--- botserver.go
+++ botserver.go
@@ -1,17 +1,13 @@
 /**
- * Copyright 2016 Tech0522
+ * Copyright 2017 recipe-linebot
  */
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
-	"path"
 	"unicode/utf8"
 
 	"github.com/line/line-bot-sdk-go/linebot"
@@ -31,68 +27,9 @@ const MaxRecipeDescLength = 60
 const RecipeDescTailIfTooLong = "..."
 const RecipeCarouselAltTextTailing = "..."
 
-type RecipeDBSearchQuery struct {
-	From  int `json:"from"`
-	Size  int `json:"size"`
-	Query struct {
-		MultiMatch struct {
-			Query  string   `json:"query"`
-			Type   string   `json:"type"`
-			Fields []string `json:"fields"`
-		} `json:"multi_match"`
-	} `json:"query"`
-}
-
-type RecipeDBSearchResult struct {
-	Hits struct {
-		Total int `json:"total"`
-		Hits  []struct {
-			Source struct {
-				Title    string `json:"title"`
-				Desc     string `json:"description"`
-				ImageUrl string `json:"image_url"`
-				Url      string `json:"detail_url"`
-			} `json:"_source"`
-		} `json:"hits"`
-	} `json:"hits"`
-}
-
 type PagingPostbackData struct {
 	From     int    `json:"from"`
 	RawQuery string `json:"rawQuery"`
-}
-
-func searchRecipes(rawQuery string, from, size int, config *RecipeLinebotConfig) (result RecipeDBSearchResult) {
-	// Build the search query
-	var query RecipeDBSearchQuery
-	query.From = from
-	query.Size = size
-	query.Query.MultiMatch.Query = rawQuery
-	query.Query.MultiMatch.Type = "cross_fields"
-	query.Query.MultiMatch.Fields = []string{"materials.keyword^100", "materials^5", "title", "description"}
-	queryAsJson, err := json.Marshal(query)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Post the search request
-	apiUrl := url.URL{Scheme: "http", Host: config.RecipeDB.Host,
-		Path: path.Join(config.RecipeDB.Index, config.RecipeDB.RecipeDoctype, "_search")}
-	resp, err := http.Post(apiUrl.String(), "application/json", bytes.NewReader(queryAsJson))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	// Interpret the response as search result
-	if resp.StatusCode != 200 {
-		body, _ := ioutil.ReadAll(resp.Body)
-		log.Fatal("Bad status code: code=" + resp.Status + " body=" + string(body))
-	}
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		log.Fatal(err)
-	}
-	return result
 }
 
 func roundRecipeDescription(desc string) string {
@@ -131,7 +68,7 @@ func newRecipesMessage(result *RecipeDBSearchResult, rawQuery string, from int) 
 			log.Fatal(err)
 		}
 		col := linebot.NewCarouselColumn(
-			fmt.Sprintf(PagingCarouselImageURL, (from / (MaxRecipesToReply - 1) % NumPagingCarouselImages)),
+			fmt.Sprintf(PagingCarouselImageURL, (from/(MaxRecipesToReply-1)%NumPagingCarouselImages)),
 			PagingCarouselTitle,
 			fmt.Sprintf(PagingCarouselText, rawQuery, result.Hits.Total-pbData.From),
 			linebot.NewPostbackTemplateAction(PagingButtonLabel, string(pbDataAsJson), ""))
